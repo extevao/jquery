008f3cc53001cb0266ae90bf4efb772f23ab51ee
Estevao <estevaovasques@hotmail.com>
2017-02-23 21:49:09 -0300

    Refatorado funcao

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,31 +1,59 @@
-var frase = $('.frase').text();
-
-var numPalavras = frase.split(" ").length;
-
-$('#tamanho-frase').text(numPalavras);
-
-
 var campo = $('.campo-digitacao');
+var tempoInicial = $("#tempo-digitacao").text();
 
-campo.on('input', function() {
-    var conteudo = campo.val();
-    var qtdPalvras = conteudo.split(' ').length;
-    $('#contador-palavras').text(qtdPalvras);
-    var qtdCaracters = conteudo.length;
-    $('#contador-caracteres').text(qtdCaracters);
-});
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
 
-var tempoRestante = $('#tempo-digitacao').text();
-campo.one("focus", function() {
-  var cronometroID = setInterval(function(){
-    tempoRestante--;
-    $('#tempo-digitacao').text(tempoRestante);
-    if(tempoRestante < 1){
-      campo.attr("disabled", true);
-      clearInterval(cronometroID);
-    }
-  }, 1000);
-});
+        var qtdPalavras = conteudo.split(/\S+/).length;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+            }
+        }, 1000);
+    });
+}
 
 
 var campo = $(".campo-digitacao");
+$("#botao-reiniciar").click(reiniciaJogo);
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    // Adicionamos aqui \/
+    inicializaCronometro();
+}
+
+
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+
+}
+
+$(function() {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
+});
